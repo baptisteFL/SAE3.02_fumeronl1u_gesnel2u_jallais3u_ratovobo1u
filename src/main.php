ba0866d081d2b6a9bdc96eb7cebad703ab3417a1
fumeronl1u <bfumeronlecomte@gmail.com>
2023-11-06 17:28:04 +0100

    ajout du logo

--- src/main.php
+++ src/main.php
@@ -5,15 +5,30 @@
     <title>Touiteur.app</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="icon" type="image/x-icon" href="images/favicon.ico">
-</head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet"></head>
 <body>
 <header>
-    <h1><a href="?action=tweet"><img src="images/logo.png" alt="logo" /></a></h1>
-    <nav>
-        <a href="?action=sign-in">Connexion</a>
-        <a href="?action=add-user">Inscription</a>
-    </nav>
+    <a href="?action=tweet"><img src="images/touiteur.png" alt="logo" /></a>
+    <a href="?action=sign-in"><p>Connexion</p></a>
+    <a href="?action=add-user"><p>Inscription</p></a>
+    <a href="?action=user-page" id="userlink"><img src="images/user.png" alt="user" id="user"/></a>
 </header>
+<!--
+<div class="tweet">
+    <div class="author">John Doe</div>
+    <div class="timestamp">2 hours ago</div>
+    <div class="content">
+        This is a sample tweet on Touiteur.app. #TouiteurApp
+    </div>
+    <div class="actions">
+        <button>Like</button>
+        <button>Retweet</button>
+    </div>
+</div>
+-->
 </body>
 </html>
 
@@ -30,15 +45,27 @@ use iutnc\touiteur\dispatch\Dispatcher;
 require_once "vendor/autoload.php";
 
 if($_SERVER['REQUEST_METHOD']=='GET'){
-    if($_GET['action']=='sign-in' || $_GET['action']=='add-user'){
-        echo "<form method='post' action=''>
+    if($_GET['action']=='sign-in'){
+        echo "
+<form method='post' action='' class='tweet' id='signin'>
+        <h1>Connectez-vous !</h1>
         <label for='email'>Email</label>
         <input type='text' name='email' id='email'><br>
         <label for='password'>Mot de passe</label>
         <input type='password' name='password' id='password'><br>
         <input type='submit' value='Envoyer'>
         </form>";
-    }elseif($_GET['action']=='tweet'){
+    }elseif ($_GET['action']=='add-user') {
+        echo "
+<form method='post' action='' class='tweet' id='signin'>
+        <h1>Inscrivez-vous !</h1>
+        <label for='email'>Email</label>
+        <input type='text' name='email' id='email'><br>
+        <label for='password'>Mot de passe</label>
+        <input type='password' name='password' id='password'><br>
+        <input type='submit' value='Envoyer'>
+        </form>";
+    } elseif($_GET['action']=='tweet'){
         echo '<div class="tweet">
             <div class="author">John Doe</div>
     <div class="timestamp">2 hours ago</div>
