95cfcfb40287297c5c192bfe346327631f4af7ef
KerrianG <gesnel2u@etu.univ-lorraine.fr>
2023-11-06 16:27:24 +0100

    Ajout du favicon.ico et changement dans le main

--- src/main.php
+++ src/main.php
@@ -4,28 +4,16 @@
     <meta charset="UTF-8">
     <title>Touiteur.app</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
 </head>
 <body>
 <header>
-    <h1><a href="?action=tweet">Bienvenue sur Touiteur.app</a></h1>
+    <h1><a href="?action=tweet"><img src="images/logo.png" alt="logo" /></a></h1>
     <nav>
         <a href="?action=sign-in">Connexion</a>
         <a href="?action=add-user">Inscription</a>
     </nav>
 </header>
-<!--
-<div class="tweet">
-    <div class="author">John Doe</div>
-    <div class="timestamp">2 hours ago</div>
-    <div class="content">
-        This is a sample tweet on Touiteur.app. #TouiteurApp
-    </div>
-    <div class="actions">
-        <button>Like</button>
-        <button>Retweet</button>
-    </div>
-</div>
--->
 </body>
 </html>
 
