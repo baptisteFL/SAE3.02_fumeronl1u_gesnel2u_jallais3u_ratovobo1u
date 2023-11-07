88e3b8a72aa2879ec53942f42c8be6afd5c085a7
KerrianG <gesnel2u@etu.univ-lorraine.fr>
2023-11-07 14:37:39 +0100

    Affichage du Feed, ajout de méthode calculernbtouitepartag.

--- src/classes/action/FeedAction.php
+++ src/classes/action/FeedAction.php
@@ -4,6 +4,8 @@ namespace iutnc\touiteur\action;
 
 use iutnc\touiteur\auth\Auth;
 use iutnc\touiteur\auth\AuthException;
+use iutnc\touiteur\db\ConnectionFactory;
+use const Exception;
 
 require_once "vendor/autoload.php";
 
@@ -12,22 +14,40 @@ class FeedAction extends Action
 
     public function execute(): string
     {
+        ConnectionFactory::makeConnection();
+        $bdd = ConnectionFactory::$bdd;
+        $req = $bdd->prepare("SELECT * FROM touite order by date desc");
         $html = "";
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-            $html .= '<div class="tweet">
-        <span id="titleTweet">
-            <div class="author">John Doe</div>
-            <div class="actions" id="follow"><button>Suivre</button></div>
-        </span>
-    <div class="timestamp">2 hours ago</div>
-    <div class="content">
-        This is a sample tweet on Touiteur.app.
-    </div>
-    <div class="tags">
-    <p class="trending">#Populaire <p id="numberTweet" class="trending">12k</p></p>
-    <p>#Exemple <p id="numberTweet">1k</p></p>    
-    </div>
-    <div class="actions">
+            try {
+                $result = $req->execute();
+                if ($result) {
+                    while ($row = $req->fetch()) {
+                        $html .= '<div class="tweet">
+                    <span id="titleTweet"> ';
+                        $req2 = $bdd->prepare("SELECT * FROM utilisateur natural join atouite where id_touite = :idTouite");
+                        $req2->bindValue(":idTouite", $row['id_touite']);
+                        $result2 = $req2->execute();
+                        if ($result2) {
+                            while ($row2 = $req2->fetch()) {
+                                $html .= '<div class="author">' . $row2['prenomUtil'] . ' ' . $row2['nomUtil'] . '</div>';
+                            }
+                        }
+                        $html .= '<div class="actions" id="follow"><button>Suivre</button></div>
+                    </span>';
+                        $html .= '<div class="timestamp">' . $row['date'] . '</div>';
+                        $html .= '<div class="content">' . $row['texte'] . '</div>';
+                        $html .= '<div class="tags">';
+                        $req3 = $bdd->prepare("SELECT * FROM tag natural join touitepartag where id_touite = :idTouite");
+                        $req3->bindValue(":idTouite", $row['id_touite']);
+                        $result3 = $req3->execute();
+                        if ($result3) {
+                            while ($row3 = $req3->fetch()) {
+                                $html .= '<p class="trending">#' . $row3['libelleTag'] . '<p id="numberTweet" class="trending">' . $this->calculerNombreTouiteParTag($row3['id_tag']) . '</p></p>';
+                            }
+                        }
+                        $html .= '</div>';
+                        $html .= '<div class="actions">
         <button id = "like">Like</button>
         <button id = "dislike">Dislike</button>
         <button>Retouite</button>
@@ -37,7 +57,32 @@ class FeedAction extends Action
     <a id="postTweet" href="?action=touite">
     <img src="images/postTweet.png" alt="post a tweet"/>
     </a>';
+                    }
+                    return $html;
+                }
+            }catch
+            (Exception $e) {
+                $html .= "<br> Vous n'avez pas accès à cet utilisateur !<br>";
+            }
         }
         return $html;
     }
+
+
+    public static function calculerNombreTouiteParTag(int $idTag): int
+    {
+        ConnectionFactory::makeConnection();
+        $bdd = ConnectionFactory::$bdd;
+        $req = $bdd->prepare("SELECT * FROM touitepartag WHERE id_tag = :idTag");
+        $req->bindValue(":idTag", $idTag);
+        $result = $req->execute();
+        $i = 0;
+        if ($result) {
+            while ($row = $req->fetch()) {
+                $i++;
+            }
+        }
+        return $i;
+    }
+
 }
