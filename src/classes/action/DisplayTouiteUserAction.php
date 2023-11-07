6de25c0c97d7f216d63f3842bae5e3e821a7574f
Nicka-R <nicka.ratovobodo8@etu.univ-lorraine.fr>
2023-11-07 15:51:58 +0100

    fonctionnalité 4 finie

--- src/classes/action/DisplayTouiteUserAction.php
+++ src/classes/action/DisplayTouiteUserAction.php
@@ -10,8 +10,41 @@ class DisplayTouiteUserAction extends Action
         {
             ConnectionFactory::makeConnection();
             $bdd = ConnectionFactory::$bdd;
-            //TODO : afficher les touites de l'utilisateur
+            //afficher les touites de l'utilisateur
             $html = "";
+            $requete = $bdd->prepare("select DISTINCT touite.id_touite, touite.texte, touite.date 
+                                    from touite, atouite, utilisateur where utilisateur.nomUtil = :nomUtil");
+            $requete->bindValue(":nomUtil", $_GET['nomUtil']);
+            $result = $requete->execute();
+            if($result){
+                while($row = $requete->fetch()){
+                    $html .= '<div class="tweet">
+                    <span id="titleTweet"> ';
+                    $html .= '<div class="author">' . $_GET['nomUtil'] . '</div>';
+                    $html .= '<div class="actions" id="follow"><button>Suivre</button></div>
+                    </span>';
+                    $html .= '<div class="timestamp">' . "Il y a " . $this->calculerDepuisQuand($row['id_touite']) . '</div>';
+                    $html .= '<div class="content">' . $row['texte'] . '</div>';
+
+                    //afficher les tags du touite
+                    $html .= '<div class="tags">';
+                    $req3 = $bdd->prepare("SELECT * FROM tag natural join touitepartag where id_touite = :idTouite");
+                    $req3->bindValue(":idTouite", $row['id_touite']);
+                    $result3 = $req3->execute();
+                    if ($result3) {
+                        while ($row3 = $req3->fetch()) {
+                            $html .= '<p class="trending">#' . $row3['libelleTag'] . '<p id="numberTweet" class="trending">' . $this->calculerNombreTouiteParTag($row3['id_tag']) . '</p></p>';
+                        }
+                    }
+                    $html .= '</div>';
+                    $html .= '<div class="actions">
+                                <button id = "like">Like</button>
+                                <button id = "dislike">Dislike</button>
+                                <button>Retouite</button>
+                            </div>
+                        </div>';
+                }
+            }
             return $html;
         }
 
