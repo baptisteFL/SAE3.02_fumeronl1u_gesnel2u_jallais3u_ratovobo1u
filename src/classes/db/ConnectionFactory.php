dd5776d4b4781c13ca39f97d70bdac9b1f7dd994
KerrianG <gesnel2u@etu.univ-lorraine.fr>
2023-11-07 14:25:17 +0100

    Modification de la connection à la base de donnée

--- src/classes/db/ConnectionFactory.php
+++ src/classes/db/ConnectionFactory.php
@@ -18,13 +18,13 @@ class ConnectionFactory{
     }
 
     public static function makeConnection(){
-        self::setConfig("db.config.ini");
-        if(empty(self::$configTab)){
-            $bdd = new \PDO("mysql:host=localhost;dbname=sae", "root", "");
+        try{
+            self::setConfig("db.config.ini");
+            $bdd = new \PDO(self::$configTab['dsn'], self::$configTab['user'], self::$configTab['password']);
             $bdd->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             self::$bdd = $bdd;
-        }else{
-            $bdd = new \PDO("mysql:host=localhost;dbname=".self::$configTab['dsn'], self::$configTab['user'], self::$configTab['password']);
+        }catch(\Exception $e){
+            $bdd = new \PDO("mysql:host=localhost;dbname=sae", "root", "");
             $bdd->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             self::$bdd = $bdd;
         }
